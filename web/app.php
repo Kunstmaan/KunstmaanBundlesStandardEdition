05d93b321e6365bca6e372bf8585c6267bdc116c
Roderik van der Veer <roderik@vanderveer.be>
2013-08-18 16:55:21 +0200

    Correct env

--- web/app.php
+++ web/app.php
@@ -8,10 +8,8 @@ $loader = require_once __DIR__.'/../app/bootstrap.php.cache';
 // Use APC for autoloading to improve performance.
 // Change 'sf2' to a unique prefix in order to prevent cache key conflicts
 // with other applications also using APC.
-/*
 $loader = new ApcClassLoader('sf2', $loader);
 $loader->register(true);
-*/
 
 require_once __DIR__.'/../app/AppKernel.php';
 
@@ -21,7 +19,7 @@ $kernel = new AppKernel('prod', false);
 //if (stripos(gethostname(), 'mytestproject.com') !== false){
 //    $kernel = new AppKernel('prod', false);
 //} else {
-//    $kernel = new AppKernel('staging', false);
+//    $kernel = new AppKernel('test', false);
 //}
 
 $kernel->loadClassCache();
--- web/app_test.php
+++ web/app_test.php
@@ -1,29 +1,23 @@
 <?php
 
+use Symfony\Component\ClassLoader\ApcClassLoader;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Debug\Debug;
-
-// If you don't want to setup permissions the proper way, just uncomment the following PHP line
-// read http://symfony.com/doc/current/book/installation.html#configuration-and-setup for more information
-umask(0000);
-
-// This check prevents access to debug front controllers that are deployed by accident to production servers.
-// Feel free to remove this, extend it, or make something more sophisticated.
-if (isset($_SERVER['HTTP_CLIENT_IP'])
-    || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
-    || !in_array(@$_SERVER['REMOTE_ADDR'], array('127.0.0.1', 'fe80::1', '::1'))
-) {
-    header('HTTP/1.0 403 Forbidden');
-    exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
-}
 
 $loader = require_once __DIR__.'/../app/bootstrap.php.cache';
-Debug::enable();
+
+// Use APC for autoloading to improve performance.
+// Change 'sf2' to a unique prefix in order to prevent cache key conflicts
+// with other applications also using APC.
+$loader = new ApcClassLoader('sf2', $loader);
+$loader->register(true);
 
 require_once __DIR__.'/../app/AppKernel.php';
-
-$kernel = new AppKernel('test', true);
+$kernel = new AppKernel('test', false);
 $kernel->loadClassCache();
+if (!isset($_SERVER['HTTP_SURROGATE_CAPABILITY']) || false === strpos($_SERVER['HTTP_SURROGATE_CAPABILITY'], 'ESI/1.0')) {
+    require_once __DIR__.'/../app/AppCache.php';
+    $kernel = new AppCache($kernel);
+}
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
 $response->send();
