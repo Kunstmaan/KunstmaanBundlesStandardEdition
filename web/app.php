588f31db60b71a93ec5f762acd949a2acaa2a2ed
Roderik van der Veer <roderik.van.der.veer@kunstmaan.be>
2016-01-19 11:09:07 +0100

    Update according to the latest Symfony 2.8 Standard Edition best practices

--- web/app.php
+++ web/app.php
@@ -1,22 +1,16 @@
 <?php
-
-use Symfony\Component\ClassLoader\ApcClassLoader;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Debug\Debug;
-
-$loader = require_once __DIR__.'/../app/bootstrap.php.cache';
+/**
+ * @var Composer\Autoload\ClassLoader
+ */
+$loader = require __DIR__.'/../app/autoload.php';
+include_once __DIR__.'/../app/bootstrap.php.cache';
 
 if (extension_loaded('apc') && ini_get('apc.enabled')) {
-    // Enable APC for autoloading to improve performance.
-    // You should change the ApcClassLoader first argument to a unique prefix
-    // in order to prevent cache key conflicts with other applications
-    // also using APC.
-    $apcLoader = new ApcClassLoader(sha1(__FILE__), $loader);
+    $apcLoader = new Symfony\Component\ClassLoader\ApcClassLoader(sha1(__FILE__), $loader);
     $loader->unregister();
     $apcLoader->register(true);
 }
-
-require_once __DIR__.'/../app/AppKernel.php';
 
 if (getenv('APP_ENV') === 'dev') {
     umask(0000);
@@ -25,7 +19,6 @@ if (getenv('APP_ENV') === 'dev') {
 } else {
     $kernel = new AppKernel('prod', false);
 }
-
 $kernel->loadClassCache();
 
 if (getenv('APP_ENV') !== 'dev') {
