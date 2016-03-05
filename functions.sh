5752c701c1e4d5ab46054433e8cf6f47fa9fed6c
Krisztián Ferenczi <ferenczi.krisztian@gmail.com>
2016-03-05 16:22:54 +0100

    Add new features to setup script
    - installed program check before install
    - ask database build workflow
    - run assetic dump in prod environment too

---
+++ functions.sh
@@ -0,0 +1,64 @@
+#!/usr/bin/env bash
+
+# Original: https://gist.github.com/JamieMason/4761049
+# Functions ==============================================
+
+# return 1 if global command line program installed, else 0
+# example
+# echo "node: $(program_is_installed node)"
+function program_is_installed {
+  # set to 1 initially
+  local return_=1
+  # set to 0 if not found
+  type $1 >/dev/null 2>&1 || { local return_=0; }
+  # return value
+  echo "$return_"
+}
+
+# return 1 if local npm package is installed at ./node_modules, else 0
+# example
+# echo "gruntacular : $(npm_package_is_installed gruntacular)"
+function npm_package_is_installed {
+  # set to 1 initially
+  local return_=1
+  # set to 0 if not found
+  ls node_modules | grep $1 >/dev/null 2>&1 || { local return_=0; }
+  # return value
+  echo "$return_"
+}
+
+# display a message in red with a cross by it
+# example
+# echo echo_fail "No"
+function echo_fail {
+  # echo first argument in red
+  printf "\e[31m✘ ${1}\e[0m"
+}
+
+# display a message in green with a tick by it
+# example
+# echo echo_fail "Yes"
+function echo_pass {
+  # echo first argument in green
+  printf "\e[32m✔ ${1}\e[0m"
+}
+
+# echo pass or fail
+# example
+# echo echo_if 1 "Passed"
+# echo echo_if 0 "Failed"
+function echo_if {
+  if [ $1 == 1 ]; then
+    echo_pass $2
+  else
+    echo_fail $2
+  fi
+}
+
+# echo a title
+# example
+# echo_title "Title of block"
+function echo_title {
+  printf "\n\n\e[97;45m\n  » ${1}\n\e[0m\n\n"
+}
+# ============================================== Functions
--- setup_project.sh
+++ setup_project.sh
@@ -1,23 +1,81 @@
 #!/usr/bin/env bash
 
+source functions.sh
+
+# ======================================================================================================================
+echo_title "First of all we are checking the software environment"
+echo "Check required programs. If you meet permission error, try run this script with $(tput setaf 3)sudo$(tput setaf 7)!"
+# Check NodeJS is exist
+if [[ $(program_is_installed node) == 0 ]]; then
+    echo "NodeJS is required, but it is not installed! Please install it before starting setup."
+    exit 1
+else
+    echo "$(echo_pass NodeJS)"
+fi
+# Check gem is exist
+if [[ $(program_is_installed gem) == 0 ]]; then
+    echo "RubyGems is required, but it is not installed! Please install it before starting setup. Help: https://rubygems.org/pages/download"
+    exit 1
+else
+    echo "$(echo_pass RubyGems)"
+fi
+# Check bundler is exist
+if [[ $(program_is_installed bundler) == 0 ]]; then
+    echo "Bundler is required, but it is not installed! Please install it before starting setup. Help: http://bundler.io/"
+    exit 1
+else
+    echo "$(echo_pass Bundler)"
+fi
+
+npms=("bower" "gulp" "uglifyjs" "uglifycss")
+for npm in "${npms[@]}"
+do
+    if [[ $(program_is_installed $npm) == 0 ]]; then
+        echo "Try to install $npm npm package..."
+        npm install -g $npm
+    else
+        echo "$(echo_pass $npm)"
+    fi
+done
+
+# ======================================================================================================================
+echo_title "Run 'app/console kuma:generate:bundle' command..."
 app/console kuma:generate:bundle
+
+echo_title "Run 'app/console kuma:generate:default-site' command..."
 app/console kuma:generate:default-site
-app/console doctrine:database:create
-app/console doctrine:schema:create
+
+echo_title "Run 'app/console doctrine:database:create' command..."
+app/console doctrine:database:create --if-not-exists
+
+echo_title "Build database..."
+echo "You can choice a workflow. You want to use database migration (create migration file and run it - preferred) OR just run 'doctrine:schema:create' command."
+read -p "Do you want to use database migration? (Y/n) " -n 1 -r
+echo
+if [[ $REPLY =~ ^[nN]$ ]]
+then
+    app/console doctrine:schema:create
+else
+    app/console doctrine:migrations:diff
+    app/console doctrine:migrations:migrate --no-interaction
+fi
+
+echo_title "Load fixtures"
 app/console doctrine:fixtures:load
 
 ## Optionaly: create admin behat tests
+echo_title "Run 'app/console kuma:generate:admin-tests' command..."
 app/console kuma:generate:admin-tests
 
-npm install -g bower
-npm install -g gulp
-npm install -g uglify-js
-npm install -g uglifycss
-
+# ======================================================================================================================
+echo_title "Install other dependencies... (Bundler, NPM, Bower, Gulp)"
 bundle install
 npm install
 bower install
 gulp build
 
+# ======================================================================================================================
+echo_title "Install and dump assets"
 app/console assets:install --symlink
 app/console assetic:dump
+app/console assetic:dump -e prod
