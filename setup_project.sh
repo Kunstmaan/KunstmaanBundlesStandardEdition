1dc1b918d44d7f08f56a03e0cc9953bd2d1a2591
Dries Beerten <dries.beerten@kunstmaan.be>
2017-05-16 12:53:40 +0200

    update setup_project script

--- setup_project.sh
+++ setup_project.sh
@@ -9,15 +9,5 @@ bin/console doctrine:fixtures:load
 ## Optionaly: create admin behat tests
 bin/console kuma:generate:admin-tests
 
-npm install -g bower
-npm install -g gulp
-npm install -g uglify-js
-npm install -g uglifycss
-
-bundle install
-npm install
-bower install
-gulp build
-
-bin/console assets:install --symlink
-bin/console assetic:dump
+## Optionaly: build all assets (FE & CMS)
+./buildUI.sh
